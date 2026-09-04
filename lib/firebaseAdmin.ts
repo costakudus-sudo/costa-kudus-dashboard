224f65fbde749f1ae62f4564336b6840ee2fe381
Costa Kudus Tech <costakudus@gmail.com>
2026-09-04 01:47:03 +0100

    Fix Firebase Admin project ID

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -5,7 +5,7 @@ const firebaseAdminApp =
   getApps().length === 0
     ? initializeApp({
         credential: cert({
-          projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+          projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
             /\\n/g,
